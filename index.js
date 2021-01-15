b69892a10537b858ae3ebf3a335b050e22012ed6
Aadarsh <aadarsh-s@outlook.com>
2021-01-15 11:56:49 +0530

    Adding CSS props

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
 #!/usr/bin/env node
-"use strict";
+'use strict';
 
-const React = require("react");
-const importJsx = require("import-jsx");
-const { render } = require("ink");
-const showBanner = require("node-banner");
+const React = require('react');
+const importJsx = require('import-jsx');
+const { render } = require('ink');
+const showBanner = require('node-banner');
 
-const ui = importJsx("./ui");
+const ui = importJsx('./ui');
 
 (async () => {
-	await showBanner(
-		"Aadarsh",
-		"Modern Full Stack Developer | System Design / Open Source Enthusiast"
-	);
+  await showBanner(
+    '@itsaadarsh',
+    'Modern Full Stack Developer | System Design / Open Source Enthusiast',
+    'cyan',
+    'green'
+  );
 })();
 
 render(React.createElement(ui));
--- ui.js
+++ ui.js
@@ -1,36 +1,58 @@
-"use strict";
-const React = require("react");
-const SelectInput = require("ink-select-input").default;
-const open = require("open");
+'use strict';
+const React = require('react');
+const SelectInput = require('ink-select-input').default;
+const { Box, Text } = require('ink');
+const open = require('open');
 
 const Counter = () => {
-	const handleSelect = (item) => {
-		open(item.value);
-	};
-	const socialsList = [
-		{
-			label: "LinkedIn",
-			value: "https://www.linkedin.com/in/itsaadarsh",
-		},
-		{
-			label: "Twitter",
-			value: "https://twitter.com/itsaadarsh_",
-		},
-		{
-			label: "Instagram",
-			value: "https://www.instagram.com/itsaadarsh/",
-		},
-		{
-			label: "Github",
-			value: "https://github.com/itsaadarsh",
-		},
-		{
-			label: "Portfolio",
-			value: "https://itsaadarsh.github.io",
-		},
-	];
+  const handleSelect = item => {
+    if (item.label === '| EXIT |') {
+      process.exit();
+    } else if (item.value) {
+      open(item.value);
+    }
+  };
 
-	return <SelectInput items={socialsList} onSelect={handleSelect} />;
+  const socialsList = [
+    {
+      label: 'LinkedIn',
+      value: 'https://www.linkedin.com/in/itsaadarsh',
+    },
+    {
+      label: 'Twitter',
+      value: 'https://twitter.com/itsaadarsh_',
+    },
+    {
+      label: 'Instagram',
+      value: 'https://www.instagram.com/itsaadarsh/',
+    },
+    {
+      label: 'Github',
+      value: 'https://github.com/itsaadarsh',
+    },
+    {
+      label: 'Portfolio',
+      value: 'https://itsaadarsh.github.io',
+    },
+    {
+      label: '--------',
+      value: 'Null1',
+    },
+    {
+      label: '| EXIT |',
+      value: 'quit action',
+    },
+    {
+      label: '--------',
+      value: 'Null2',
+    },
+  ];
+
+  return (
+    <Box justifyContent='center' borderStyle='round' borderColor='cyanBright' width='50%'>
+      <SelectInput items={socialsList.map(ele => ({ ...ele, key: ele.value }))} onSelect={handleSelect} />
+    </Box>
+  );
 };
 
 module.exports = Counter;
