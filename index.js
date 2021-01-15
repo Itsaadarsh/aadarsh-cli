83bb8a7cec904aa95cd028d2ed6cdf6ebafc6207
Aadarsh <aadarsh-s@outlook.com>
2021-01-15 11:21:30 +0530

    cleared NPM ERR bugs

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-require("./src/cli");
+#!/usr/bin/env node
+"use strict";
+
+const React = require("react");
+const importJsx = require("import-jsx");
+const { render } = require("ink");
+const showBanner = require("node-banner");
+
+const ui = importJsx("./ui");
+
+(async () => {
+	await showBanner(
+		"Aadarsh",
+		"Modern Full Stack Developer | System Design / Open Source Enthusiast"
+	);
+})();
+
+render(React.createElement(ui));
